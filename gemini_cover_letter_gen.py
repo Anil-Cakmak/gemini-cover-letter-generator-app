64a8d47e5f3d29ad848757f93aa9b0b36ace65d0
Anıl Çakmak <anilcakmak0506@gmail.com>
2024-11-23 14:37:26 +0300

    updated user interface.

--- gemini_cover_letter_gen.py
+++ gemini_cover_letter_gen.py
@@ -1,17 +1,25 @@
 from helpers import *
 import streamlit as st
 
-st.title("Cover Letter Generator")
+st.title("AI-Driven Cover Letter Builder")
+
+st.write("""
+To use this app, you need a Gemini API key. Please follow the steps below if you don't have one:
+
+1. [Get a Gemini API key in Google AI Studio](https://aistudio.google.com/app/apikey?_gl=1*1elzb2j*_ga*Nzg0MDg5MzQ5LjE3MzIwNDcxOTk.*_ga_P1DBVKWT6V*MTczMjM1ODk3Ny4xOS4xLjE3MzIzNTk1MDguMjQuMC4xMzEwOTE1NTgy).
+2. Follow the instructions to generate your API key.
+3. Once you have your API key, come back to this app and enter it below.
+""")
 
 st.markdown(
     "<h1 style='color:red; text-transform: uppercase; font-size: 20px; '>Do not submit sensitive, confidential, or personal information!</h1>",
     unsafe_allow_html=True
 )
-st.write("Fill in the details below to generate a customized, professional cover letter.")
+st.write("Submit the details below to receive a custom, professional cover letter.")
 
-job_title = st.text_input("Enter job title *")
-company_name = st.text_input("Enter company name *")
-job_description = st.text_area("Enter your job description *")
+job_title = st.text_input("Enter the job title *")
+company_name = st.text_input("Enter the company name *")
+job_description = st.text_area("Enter the job description *")
 cv = uploaded_file = st.file_uploader("""Upload your CV in PDF format *     (REMOVE OR MASK ANY PERSONAL INFORMATION FROM YOUR CV BEFORE UPLOADING!)""",
                                         type="pdf")
 
